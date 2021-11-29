ce4bffde2d6a0b8a9ca375ea3b1e032fe4020339
Hrishikesh <hrishikesh.vaze01@gmail.com>
2021-11-29 12:46:21 +0530

    hpc lab 2 completed

--- HPC/lab2/code.cpp
+++ HPC/lab2/code.cpp
@@ -5,26 +5,28 @@
  */
 
 #include <iostream>
+#include <omp.h>
+#include <pthread.h>
+#include <iomanip>
 using namespace std;
 
-void print_matrix(int a[][3], int n)
-{
-    cout << endl;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+#define MAX_ROWS 500 // max rows of matrix
+#define MAX_COLS 100 // max cols of matrix
+#define TWIDTH 30    // width of table
 
-void multiply_matrix(int a[][3], int b[][3], int c[][3], int n)
+double t_start, t_end;               // to store start and end time
+int num_cores = omp_get_num_procs(); // no of cpu cores
+
+int mat1[MAX_ROWS][MAX_COLS] = {0}; // matA
+int mat2[MAX_ROWS][MAX_COLS] = {0}; // matB
+int mat3[MAX_ROWS][MAX_COLS] = {0}; // matC
+
+// serial multiplication
+void multiply_matrix_serial(int a[][MAX_COLS], int b[][MAX_COLS], int c[][MAX_COLS], int n)
 {
+    t_start = omp_get_wtime();
     for (int i = 0; i < n; i++)
     {
-        #pragma omp parallel for
         for (int j = 0; j < n; j++)
         {
             c[i][j] = 0;
@@ -34,20 +36,63 @@ void multiply_matrix(int a[][3], int b[][3], int c[][3], int n)
             }
         }
     }
+    t_end = omp_get_wtime();
+}
+
+// parallel multiplication
+void multiply_matrix_parallel(int a[][MAX_COLS], int b[][MAX_COLS], int c[][MAX_COLS], int n)
+{
+    t_start = omp_get_wtime();
+
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            c[i][j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    t_end = omp_get_wtime();
+}
+
+string get_time_difference()
+{
+    return (to_string((t_end - t_start) * 1000) + " ms");
+}
+
+void calculate_diff(int n)
+{
+    int data_points = n * MAX_COLS;
+    cout << endl
+         << setw(TWIDTH) << data_points;
+    // normal serial
+    multiply_matrix_serial(mat1, mat2, mat3, n);
+
+    cout << setw(TWIDTH) << get_time_difference();
+
+    // parallel with default cores
+    multiply_matrix_parallel(mat1, mat2, mat3, n);
+    cout << setw(TWIDTH) << get_time_difference();
+
+    // parallel with all cores
+    omp_set_num_threads(num_cores); // use all cores
+    multiply_matrix_parallel(mat1, mat2, mat3, n);
+    cout << setw(TWIDTH) << get_time_difference();
 }
 
 int main()
 {
-    int mat1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int mat2[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int mat3[3][3] = {0};
+    cout << endl
+         << setw(TWIDTH) << "data points" << setw(TWIDTH) << "serial" << setw(TWIDTH) << "parallel (default cores)" << setw(TWIDTH) << "parallel (all cores)";
+    for (int i = 10; i < MAX_ROWS; i = i + 10)
+    {
+        calculate_diff(i);
+    }
 
-    print_matrix(mat1, 3);
-    print_matrix(mat2, 3);
-
-    multiply_matrix(mat1, mat2, mat3, 3);
-
-    print_matrix(mat3, 3);
     cout << endl;
     return 0;
 }
